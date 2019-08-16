cbe99e30b5835198c6eafefe5b1d087a3b2a3a73
jiujue <2936435008@qq.com>
2019-08-16 08:46:23 +0800

    first function finish

--- wechat/views.py
+++ wechat/views.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+
+import hashlib
 
 # Create your views here.
+
+def test(request):
+
+    print(request.GET['name'])
+
+    wechat_token = settings.WECHAT_TOKEN
+
+    return HttpResponse(wechat_token+request.GET['name'])
+
+def wechat_auth(request):
+
+    try :
+        signature = request.GET['signature']
+        timestamp = request.GET['timestamp']
+        nonce = request.GET['nonce']
+        echostr = request.GET['echostr']
+    except Exception as e:
+        signature = timestamp = nonce = echostr = None
+
+
+    if None in  [signature,timestamp,nonce,echostr] :
+        return HttpResponse('refusal accept connect')
+
+
+    wechat_token = settings.WECHAT_TOKEN if settings.WECHAT_TOKEN != None else 'jiujue'
+    argument = [timestamp,nonce,wechat_token]
+    has1_res = hashlib.sha1(''.join(argument.sort())).hexdigest()
+
+    if has1_res == signature:
+        return HttpResponse(echostr)
+
+    return HttpResponse('signature have difference')
